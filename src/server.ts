e1edeab283e3f09fc6a307040d6b50ae72335f7d
Icaro Ramos <polosnramos@hotmail.com>
2024-04-25 10:21:05 -0300

    feat: Add and configure Knex in the project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
+import { knex } from "./database";
 
 const app = fastify();
+
+app.get("/test", async () => {
+  const test = await knex("sqlite_scheme").select("*");
+  return test;
+});
 
 app
   .listen({
